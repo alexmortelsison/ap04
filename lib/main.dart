59d53b197c50b57c4a70e6e8ed0ce9267cb3fdd0
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-06-05 18:55:59 +0900

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:ap04/firebase_options.dart';
 import 'package:ap04/themes/lightmode.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/login_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
