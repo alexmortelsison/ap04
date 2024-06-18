942c9d05d3ae1ef6b779301d036d0a1d3da2c1cc
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-06-18 15:40:47 +0900

    fixed bug

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,9 @@
 import 'package:ap04/components/my_button.dart';
 import 'package:ap04/components/my_textfield.dart';
 import 'package:ap04/components/square_tile.dart';
+import 'package:ap04/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
   final void Function()? onTap;
@@ -14,126 +16,160 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+
+  void signUserIn(String? docID) async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+    try {
+      FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      Navigator.pop(context);
+      showErrorMessage(e.code);
+    }
+  }
+
+  void showErrorMessage(String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Text(message),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Icon(
-              Icons.lock,
-              size: 100,
-              color: Theme.of(context).colorScheme.inversePrimary,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 150),
+            Center(
+              child: Icon(
+                Icons.lock,
+                size: 100,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
             ),
-          ),
-          const SizedBox(height: 50),
-          Text(
-            'You\'ve been missed. Login now.',
-            style: TextStyle(
-              color: Theme.of(context).colorScheme.inversePrimary,
+            const SizedBox(height: 50),
+            Text(
+              'You\'ve been missed. Login now.',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
             ),
-          ),
-          const SizedBox(height: 50),
-          MyTextfield(
-            controller: emailController,
-            obscureText: false,
-            hintText: 'Email',
-          ),
-          const SizedBox(height: 10),
-          MyTextfield(
-            controller: passwordController,
-            obscureText: true,
-            hintText: 'Password',
-          ),
-          const SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  'Forgot Password?',
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.inversePrimary,
-                  ),
-                ),
-              ],
+            const SizedBox(height: 50),
+            MyTextfield(
+              controller: emailController,
+              obscureText: false,
+              hintText: 'Email',
             ),
-          ),
-          const SizedBox(height: 50),
-          MyButton(
-            onTap: () {},
-            text: 'Sign In',
-          ),
-          const SizedBox(height: 50),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Divider(
-                    thickness: 0.5,
-                    color: Theme.of(context).colorScheme.inversePrimary,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 4),
-                  child: Text(
-                    'Or continue with',
+            const SizedBox(height: 10),
+            MyTextfield(
+              controller: passwordController,
+              obscureText: true,
+              hintText: 'Password',
+            ),
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Forgot Password?',
                     style: TextStyle(
                       color: Theme.of(context).colorScheme.inversePrimary,
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Divider(
-                    thickness: 0.5,
-                    color: Theme.of(context).colorScheme.inversePrimary,
+                ],
+              ),
+            ),
+            const SizedBox(height: 50),
+            MyButton(
+              onTap: () => signUserIn(null),
+              text: 'Sign In',
+            ),
+            const SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                    ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4),
+                    child: Text(
+                      'Or continue with',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SquareTile(
+                  onTap: () => AuthService().signInWithGoogle(),
+                  imagePath: 'images/1.png',
+                ),
+                const SizedBox(width: 10),
+                SquareTile(
+                  onTap: () {},
+                  imagePath: 'images/2.png',
                 ),
               ],
             ),
-          ),
-          const SizedBox(height: 50),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SquareTile(
-                onTap: () {},
-                imagePath: 'images/1.png',
-              ),
-              const SizedBox(width: 10),
-              SquareTile(
-                onTap: () {},
-                imagePath: 'images/2.png',
-              ),
-            ],
-          ),
-          const SizedBox(height: 50),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Not yet a member? ',
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.inversePrimary,
-                ),
-              ),
-              GestureDetector(
-                onTap: widget.onTap,
-                child: const Text(
-                  'Register Now',
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Not yet a member? ',
                   style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.inversePrimary,
                   ),
                 ),
-              )
-            ],
-          )
-        ],
+                GestureDetector(
+                  onTap: widget.onTap,
+                  child: const Text(
+                    'Register Now',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  signInWithGoogle() async {
+    final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+    final credentials = GoogleAuthProvider.credential(
+        idToken: gAuth.idToken, accessToken: gAuth.accessToken);
+
+    return await FirebaseAuth.instance.signInWithCredential(credentials);
+  }
+}
